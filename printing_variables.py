6ed71af785b58fe4e8c6e80372d927424d4749ff
hemlatachandrakar03DA <hemlatachandrakar03@gmail.com>
2025-11-05 17:33:34 +0530

    how to print variables

---
+++ printing_variables.py
@@ -0,0 +1,13 @@
+#name="Hemlata Chandrakar"
+#age="24"
+#gender="female"
+#print("my name is",name)
+#print("my age is",24)
+#print("I am",gender) 
+
+name="Hemlata Chandrakar"
+age="24"
+gender="female"
+print("my name is",name,"my age is",age,"I am",gender")
+
+
