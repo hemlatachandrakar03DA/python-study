d34dcc642c7851479086214f775a68f0b9f2bc5f
hemlatachandrakar03DA <hemlatachandrakar03@gmail.com>
2025-11-08 20:37:30 +0530

    question and the solution of the problem

---
+++ question.py
@@ -0,0 +1,58 @@
+#question=4 - 10
+
+#4-write a python program to add two numbers entered by the User 
+
+"""num1=int(input("enter first number="))
+num2=int(input("enter second number="))
+total=num1+num2
+print(total)
+print(10f"total number of {num1} and {num2} is {total}")"""
+#==============================================================================
+
+
+#5-convert a string to an integer and vice versa
+
+"""number_string="556"
+print(number_string,type(number_string))
+
+number=int(number_string)
+print(number,type(number))"""
+#==============================================================================
+
+#6-write a python program to calculate the area of rectangle using user input for length and width
+
+"""lenth=float(input("enter the length is="))
+width=float(input("enter the width is="))
+
+area=lenth*width
+print(f"the area of rectangle is{area}")"""
+#===========================================================================
+
+#7-write a python program to calculate the average of three numbers entered by the user
+
+"""num1=int(input("enter the first number="))
+num2=int(input("enter the second number="))
+num3=int(input("enter the third number="))
+
+average=(num1+num2+num3)/3
+print(f"the average of three number is {average}")"""
+#===============================================================================
+
+#9-write a program that converts a temperate in fahrenhit to celcius.the formula is celcius=(fahrenhit-32)*5/9
+
+"""f=float(input('enter the farenhite number='))
+celcius=(f-32)*5/9
+print(f"the fahrenhit-{f}=the celcius-{celcius}")"""
+#=============================================================================
+
+#10-calculate the sum of 5 subjects and find percantage
+
+hindi=int(input("enter the number="))
+english=int(input("enter the number="))
+science=int(input("enter the number"))
+maths=int(input("enter the number="))
+computer=int(input("enter the number="))
+total=hindi+english+science+maths+computer
+print(total)
+percentage=(total/500)*100
+print(f"the percantage is {percentage}")
