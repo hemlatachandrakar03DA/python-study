537ff1396db541a57da418f2ba22d2002f70b788
hemlatachandrakar03DA <hemlatachandrakar03@gmail.com>
2025-11-08 19:49:47 +0530

    how to use and working of input function

---
+++ input_function.py
@@ -0,0 +1,28 @@
+"""x=input("type something=a")
+print("you entered")
+print(x)"""
+
+
+"""num1=input("enter first number=100")
+num2=input("enter second number=200")
+
+total=num1+num2
+print(total)"""
+
+"""num1=input("enter first number=100")
+num2=input("enter second number=200")
+x=int(num1)
+y=int(num2)
+total=x+y
+print(total)"""
+
+name=input("enter the name=")
+gender=input("enter the gender=")
+
+print(f"your name is {name}")
+print(f"you gender is {gender}")
+
+"""num1=int(input("enter first number="))
+num2=int(input("enter second number=100"))
+total=num1+num2
+print(total)"""
