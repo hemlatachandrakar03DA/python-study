1aa5174a7a1f6abe024262822a44ea8f49e23fd8
hemlatachandrakar03DA <hemlatachandrakar03@gmail.com>
2025-11-09 19:09:43 +0530

    how the arithmetic operations wprks

---
+++ arithmetic_operators.py
@@ -0,0 +1,17 @@
+#arthimetic operation
+
+"""a=10
+b=4
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a**b)
+print(a%b)
+print(a//b)"""
+#===================================================================
+
+a=5
+b=0
+print(a/b)
+
