6ac9cf44bb5ac6399095a25cb976503fae273f1a
hemlatachandrakar03DA <hemlatachandrakar03@gmail.com>
2025-11-09 19:35:47 +0530

    how the comparison operator works

---
+++ comparison_operators.py
@@ -0,0 +1,31 @@
+#comparison opearators
+
+#answer is always yes/no means true/false (boolean).
+
+"""a=20
+b=10
+
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b)
+print(a!=b)"""
+#===============================================
+
+#00-a and b eqal or not
+
+"""a=20
+b=21
+print(a==b)"""
+#==============================================
+
+#01-a and b euqal or not
+
+a=20
+b=20
+print(a!=b)
+
+
+
+
