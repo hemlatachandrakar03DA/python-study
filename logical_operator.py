b85e2f7fbcd8f7d525485e900641ee8d1efb5164
hemlatachandrakar03DA <hemlatachandrakar03@gmail.com>
2025-11-09 20:03:18 +0530

    how the logical operator works

---
+++ logical_operator.py
@@ -0,0 +1,34 @@
+#logical operator
+
+#and= returns true if both the statement are true
+
+"""a=55
+b=57
+print(a<b)
+print(b>a)
+
+print(a>b)
+
+print((a>b)and(b<a))"""
+#==============================================
+
+#or= returns true if one of the statement is true
+
+"""a=33
+b=33
+print(a>b)
+print(a<b)
+
+print(a==b)
+
+print((a>b)or(a==b))"""
+#===============================================
+
+#not= reverse if the result false if the result is true
+
+a=20
+b=25
+print(not(a==b)and(a<b))
+
+
+
